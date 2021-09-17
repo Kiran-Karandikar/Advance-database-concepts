8fdab7bdfe84a401106ea518ce32da49e0a87a55
Kiran Karandikar <no-reply-kiran-karandikar@git.com>
2021-09-17 11:33:08 -0400

    Assignment-2 WIP

---
+++ Assignememt-2/Submission/assignment-2.sql
@@ -0,0 +1,324 @@
+-- Script for Assignment 2
+
+-- Creating database with full name
+
+CREATE DATABASE kiran_karandikar;
+
+-- Connecting to database 
+\c kiran_karandikar;
+
+-- Relation schemas and instances for assignment 2
+CREATE TABLE Person(pid integer,
+                    pname text,
+                    city text,
+                    primary key (pid));
+
+CREATE TABLE Company(cname text,
+                     headquarter text,
+                     primary key (cname));
+
+CREATE TABLE Skill(skill text,
+                   primary key (skill));
+
+
+CREATE TABLE worksFor(pid integer,
+                      cname text,
+                      salary integer,
+                      primary key (pid),
+                      foreign key (pid) references Person (pid),
+                      foreign key (cname) references Company(cname));
+
+
+CREATE TABLE companyLocation(cname text,
+                             city text,
+                             primary key (cname, city),
+                             foreign key (cname) references Company (cname));
+
+
+CREATE TABLE personSkill(pid integer,
+                         skill text,
+                         primary key (pid, skill),
+                         foreign key (pid) references Person (pid) on delete cascade,
+                         foreign key (skill) references Skill (skill) on delete cascade);
+
+
+CREATE TABLE hasManager(eid integer,
+                        mid integer,
+                        primary key (eid, mid),
+                        foreign key (eid) references Person (pid),
+                        foreign key (mid) references Person (pid));
+
+CREATE TABLE Knows(pid1 integer,
+                   pid2 integer,
+                   primary key(pid1, pid2),
+                   foreign key (pid1) references Person (pid),
+                   foreign key (pid2) references Person (pid));
+
+
+
+INSERT INTO Person VALUES
+     (1001,'Jean','Cupertino'),
+     (1002,'Vidya', 'Cupertino'),
+     (1003,'Anna', 'Seattle'),
+     (1004,'Qin', 'Seattle'),
+     (1005,'Megan', 'MountainView'),
+     (1006,'Ryan', 'Chicago'),
+     (1007,'Danielle','LosGatos'),
+     (1008,'Emma', 'Bloomington'),
+     (1009,'Hasan', 'Bloomington'),
+     (1010,'Linda', 'Chicago'),
+     (1011,'Nick', 'MountainView'),
+     (1012,'Eric', 'Cupertino'),
+     (1013,'Lisa', 'Indianapolis'), 
+     (1014,'Deepa', 'Bloomington'), 
+     (1015,'Chris', 'Denver'),
+     (1016,'YinYue', 'Chicago'),
+     (1017,'Latha', 'LosGatos'),
+     (1018,'Arif', 'Bloomington'),
+     (1019,'John', 'NewYork');
+
+INSERT INTO Company VALUES
+     ('Apple', 'Cupertino'),
+     ('Amazon', 'Seattle'),
+     ('Google', 'MountainView'),
+     ('Netflix', 'LosGatos'),
+     ('Microsoft', 'Redmond'),
+     ('IBM', 'NewYork'),
+     ('ACM', 'NewYork'),
+     ('Yahoo', 'Sunnyvale');
+
+
+INSERT INTO worksFor VALUES
+     (1001,'Apple', 65000),
+     (1002,'Apple', 45000),
+     (1003,'Amazon', 55000),
+     (1004,'Amazon', 55000),
+     (1005,'Google', 60000),
+     (1006,'Amazon', 55000),
+     (1007,'Netflix', 50000),
+     (1008,'Amazon', 50000),
+     (1009,'Apple',60000),
+     (1010,'Amazon', 55000),
+     (1011,'Google', 70000), 
+     (1012,'Apple', 50000),
+     (1013,'Yahoo', 55000),
+     (1014,'Apple', 50000), 
+     (1015,'Amazon', 60000),
+     (1016,'Amazon', 55000),
+     (1017,'Netflix', 60000),
+     (1018,'Apple', 50000),
+     (1019,'Microsoft', 50000);
+
+INSERT INTO companyLocation VALUES
+   ('Apple', 'Bloomington'),
+   ('Amazon', 'Chicago'),
+   ('Amazon', 'Denver'),
+   ('Amazon', 'Columbus'),
+   ('Google', 'NewYork'),
+   ('Netflix', 'Indianapolis'),
+   ('Netflix', 'Chicago'),
+   ('Microsoft', 'Bloomington'),
+   ('Apple', 'Cupertino'),
+   ('Amazon', 'Seattle'),
+   ('Google', 'MountainView'),
+   ('Netflix', 'LosGatos'),
+   ('Microsoft', 'Redmond'),
+   ('IBM', 'NewYork'),
+   ('Yahoo', 'Sunnyvale');
+
+INSERT INTO Skill VALUES
+   ('Programming'),
+   ('AI'),
+   ('Networks'),
+   ('OperatingSystems'),
+   ('Databases');
+
+INSERT INTO personSkill VALUES
+ (1001,'Programming'),
+ (1001,'AI'),
+ (1002,'Programming'),
+ (1002,'AI'),
+ (1004,'AI'),
+ (1004,'Programming'),
+ (1005,'AI'),
+ (1005,'Programming'),
+ (1005,'Networks'),
+ (1006,'Programming'),
+ (1006,'OperatingSystems'),
+ (1007,'OperatingSystems'),
+ (1007,'Programming'),
+ (1009,'OperatingSystems'),
+ (1009,'Networks'),
+ (1010,'Networks'),
+ (1011,'Networks'),
+ (1011,'OperatingSystems'),
+ (1011,'AI'),
+ (1011,'Programming'),
+ (1012,'AI'),
+ (1012,'OperatingSystems'),
+ (1012,'Programming'),
+ (1013,'Programming'),
+ (1013,'OperatingSystems'),
+ (1013,'Networks'),
+ (1014,'OperatingSystems'),
+ (1014,'AI'),
+ (1014,'Networks'),
+ (1015,'Programming'),
+ (1015,'AI'),
+ (1016,'OperatingSystems'),
+ (1016,'AI'),
+ (1017,'Networks'),
+ (1017,'Programming'),
+ (1018,'AI'),
+ (1019,'Networks');
+
+INSERT INTO hasManager VALUES
+ (1004, 1003),
+ (1006, 1003),
+ (1015, 1003),
+ (1016, 1004),
+ (1016, 1006),
+ (1008, 1015),
+ (1010, 1008),
+ (1013, 1007),
+ (1017, 1013),
+ (1002, 1001),
+ (1009, 1001),
+ (1014, 1012),
+ (1011, 1005);
+
+
+INSERT INTO Knows VALUES
+ (1011,1009),
+ (1007,1016),
+ (1011,1010),
+ (1003,1004),
+ (1006,1004),
+ (1002,1014),
+ (1009,1005),
+ (1018,1009),
+ (1007,1017),
+ (1017,1019),
+ (1019,1013),
+ (1016,1015),
+ (1001,1012),
+ (1015,1011),
+ (1019,1006),
+ (1013,1002),
+ (1018,1004),
+ (1013,1007),
+ (1014,1006),
+ (1004,1014),
+ (1001,1014),
+ (1010,1013),
+ (1010,1014),
+ (1004,1019),
+ (1018,1007),
+ (1014,1005),
+ (1015,1018),
+ (1014,1017),
+ (1013,1018),
+ (1007,1008),
+ (1005,1015),
+ (1017,1014),
+ (1015,1002),
+ (1018,1013),
+ (1018,1010),
+ (1001,1008),
+ (1012,1011),
+ (1002,1015),
+ (1007,1013),
+ (1008,1007),
+ (1004,1002),
+ (1015,1005),
+ (1009,1013),
+ (1004,1012),
+ (1002,1011),
+ (1004,1013),
+ (1008,1001),
+ (1008,1019),
+ (1019,1008),
+ (1001,1019),
+ (1019,1001),
+ (1004,1003),
+ (1006,1003),
+ (1015,1003),
+ (1016,1004),
+ (1016,1006),
+ (1008,1015),
+ (1010,1008),
+ (1017,1013),
+ (1002,1001),
+ (1009,1001),
+ (1011,1005),
+ (1014,1012);
+
+
+
+create table tree(parent integer,
+                  child integer);
+
+insert into tree values
+ (1,2),
+ (1,3),
+ (1,4),
+ (2,5),
+ (2,6),
+ (3,7),
+ (5,8),
+ (8,9),
+ (8,10),
+ (8,11),
+ (7,12),
+ (7,13),
+ (12,14),
+ (14,15);
+ 
+ \qecho 'Problem 1'
+
+-- Consider the query “Find the pid and name of each person who works
+-- for Google and who has a higher salary than some other person who he
+-- or she knows and who also works for Google.”
+
+-- (a) Formulate this query in SQL by only using the EXISTS or NOT EXISTS set predicates.
+
+select distinct p.pid , p.pname from person p, worksfor w where exists (select 1 from worksfor wf1 where wf1.cname='Google' and wf1.pid=p.pid) and exists (SELECT 1
+	FROM knows kn where kn.pid1=p.pid and exists(select 1 from worksfor wf where wf.cname='Google' and wf.pid=kn.pid2 and wf.salary < w.salary));
+
+-- (b) Formulate this query in SQL by only using the IN, NOT IN, SOME,
+-- or ALL set membership predicates.
+
+select distinct p.pid , p.pname from person p, worksfor w where p.pid in (select w.pid from worksfor w where w.cname='Google') and p.pid in (SELECT kn.pid1 FROM knows kn where kn.pid2 in (select wf.pid from worksfor wf where wf.cname='Google' and wf.salary < w.salary));
+
+
+-- (c) Formulate this query in SQL without using set predicates.
+select distinct p.pid , p.pname from person p, worksfor w where w.cname='Google' and w.pid=p.pid
+Intersect
+select distinct p1.pid , p1.pname from person p1, person p2, worksfor wf1, worksfor wf2, knows kn  where wf1.cname='Google' and wf1.pid=p1.pid and kn.pid1=p1.pid and kn.pid2=p2.pid and wf1.salary > wf2.salary and wf2.cname='Google'
+
+\qecho 'Problem 2'
+
+-- Consider the query “Find the cname of each company with headquarter in
+-- Cupertino, but that is not located in Indianapolis, along with the
+-- pid, name, and salary of each person who works for that company and
+-- who has the next-to-lowest salary at that company.
+
+-- (a) Formulate this query in SQL by only using the EXISTS or NOT EXISTS
+-- set predicates. You can not use the set operations INTERSECT, UNION,
+-- and EXCEPT.
+
+
+-- (b) Formulate this query in SQL by only using the IN, NOT IN, SOME, or
+-- ALL set membership predicates. You can not use the set oper- ations
+-- INTERSECT, UNION, and EXCEPT.
+
+
+-- (c) Formulate this query in SQL without using set predicates.
+
+\qecho 'Connect to default database'
+
+\c postgres;
+
+-- \qecho 'Drop database created for this assignment'
+
+-- DROP DATABASE kiran_karandikar;
